4ba379c60c42a3deeda6389ae91b43665f46d3c8
Ted Shumway <shumway.ted@gmail.com>
2015-11-24 09:46:58 -0800

    finished the passport

--- models/user.js
+++ models/user.js
@@ -1,30 +1,21 @@
 var mongoose = require('mongoose'),
 		Schema = mongoose.Schema,
-		// bcrypt = require('bcrypt'),
-	  userSchema = new Schema({
+		bcrypt = require('bcrypt')
+
+	var userSchema = new Schema({
+		local:	{
 			name: String,
 			email: {type: String, require: true},
 			password: {type: String, require: true},
 			zip: String
-}
-			facebook: {
+},
+	facebook: {
         id: String,
         name: String,
         token: String,
         email: String
+			}
     })
-
-// userSchema.pre('save', function(next){
-// 	var user = this
-// 	var hash = bcrypt.hashSync(user.password, 8)
-// 	user.password = hash
-// 	next()
-// })
-
-// userSchema.methods.authenticate = function(password){
-// 	var user = this
-// 	return bcrypt.compareSync(password, user.password)
-// }
 
 /*Ted Added passport*/
 userSchema.methods.generateHash = function(password){
