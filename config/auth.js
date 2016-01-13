2b97ea62385b10242c86e4162c8e38c3559bd2fe
Colin <clnsdabst@gmail.com>
2016-01-12 22:43:30 -0800

    styled profile page. changed fb auth route.

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': '524998997660640',
         'clientSecret': '39cfafbd44877d10f0efa8b13ef5ee18',
-        'callbackURL': 'https://hidden-earth-8076.herokuapp.com/auth/facebook/callback',
+        'callbackURL': 'http://soundseeker.herokuapp.com/auth/facebook/callback',
         'profileFields': ['emails', 'displayName']
     }
 }
